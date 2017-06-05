6aff7c661816e97b9fb85a7f5f5bf7a37f34312b
jnelson1 <jnelson1@hwemail.com>
2017-06-05 21:51:01 +0200

    Add files via upload

---
+++ ixLocationTwo/FirstViewController.swift
@@ -0,0 +1,57 @@
+//
+//  FirstViewController.swift
+//  ixLocationTwo
+//
+//  Created by Jon Nelson1 on 6/5/17.
+//  Copyright © 2017 Jon Nelson. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class FirstViewController: UIViewController, CLLocationManagerDelegate{
+    
+    @IBOutlet weak var map: MKMapView!
+    var locationManager: CLLocationManager!
+    var currentUserLocation: CLLocation!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        //locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        // Get the users location from the array of locations
+        let userLocation: CLLocation = locations[0] as CLLocation
+        
+        // You can call stopUpdatingLocation() to stop listening for location updates
+        // manager.stopUpdatingLocation()
+        
+        print("user latitude = \(userLocation.coordinate.latitude)")
+        print("user longitude = \(userLocation.coordinate.longitude)")
+        
+        // Store reference to the users location in the class instance (self)
+        self.currentUserLocation = userLocation
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error)
+    {
+        // An error occurred trying to retrieve users location
+        print("Error \(error)")
+    }
+}
+
---
+++ ixLocationTwo/SecondViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.swift
+//  ixLocationTwo
+//
+//  Created by Jon Nelson1 on 6/5/17.
+//  Copyright © 2017 Jon Nelson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func unwindToViewController (sender: UIStoryboardSegue){
+        
+    }
+
+}
+
